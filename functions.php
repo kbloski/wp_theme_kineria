8ad9da672e74343fd5650b810a96a247c4371ce2
Kamil Błoński <kblonski02@gmail.com>
2025-06-05 23:40:52 -0800

    Update functions

--- functions.php
+++ functions.php
@@ -1,20 +1,28 @@
 <?php
-/**
- * 
- * @package WordPress
- * @subpackage 
- * @since 
- * 
- * @author Kamil Błoński
- */
 
+function neom_child_theme_enqueue_styles() {
+	// Najpierw styl motywu nadrzędnego
+	wp_enqueue_style(
+		'neom-city-parent-style',
+		get_template_directory_uri() . '/style.css'
+	);
 
-function enqueue_child_theme_styles() {
-    wp_enqueue_style(
-        'child-style',
-        get_stylesheet_uri(),
-        array(),
-        wp_get_theme()->get('Version')
-    );
+	// Teraz styl motywu potomnego
+	wp_enqueue_style(
+		'neom-city-child-style',
+		get_stylesheet_directory_uri() . '/style.css',
+		array( 'neom-city-parent-style' ),
+		wp_get_theme()->get('Version')
+	);
+
+	// Styl inline z kolorami (jeśli chcesz nadpisać kolory)
+	// $custom_css = '
+    // :root {
+    // }';
+	// wp_add_inline_style( 'neom-city-child-style', $custom_css );
 }
-add_action('wp_enqueue_scripts', 'enqueue_child_theme_styles');
+add_action( 'wp_enqueue_scripts', 'neom_child_theme_enqueue_styles', 20 );
+
+
+// Hook the enqueue styles function with a higher priority.
+add_action( 'wp_enqueue_scripts', 'neom_child_theme_enqueue_styles', 20 );
