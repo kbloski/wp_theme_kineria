96b18be9dfe3d18d46d19acebf5fde83a883c6a1
Kamil Błoński <kblonski02@gmail.com>
2025-09-25 12:39:27 -0700

    update

--- functions.php
+++ functions.php
@@ -1,28 +1,28 @@
-<?php
-
-function neom_child_theme_enqueue_styles() {
-	// Najpierw styl motywu nadrzędnego
-	wp_enqueue_style(
-		'neom-city-parent-style',
-		get_template_directory_uri() . '/style.css'
-	);
-
-	// Teraz styl motywu potomnego
-	wp_enqueue_style(
-		'neom-city-child-style',
-		get_stylesheet_directory_uri() . '/style.css',
-		array( 'neom-city-parent-style' ),
-		wp_get_theme()->get('Version')
-	);
-
-	// Styl inline z kolorami (jeśli chcesz nadpisać kolory)
-	// $custom_css = '
-    // :root {
-    // }';
-	// wp_add_inline_style( 'neom-city-child-style', $custom_css );
-}
-add_action( 'wp_enqueue_scripts', 'neom_child_theme_enqueue_styles', 20 );
-
-
-// Hook the enqueue styles function with a higher priority.
-add_action( 'wp_enqueue_scripts', 'neom_child_theme_enqueue_styles', 20 );
+<?php
+
+function neom_child_theme_enqueue_styles() {
+	// Najpierw styl motywu nadrzędnego
+	wp_enqueue_style(
+		'neom-city-parent-style',
+		get_template_directory_uri() . '/style.css'
+	);
+
+	// Teraz styl motywu potomnego
+	wp_enqueue_style(
+		'neom-city-child-style',
+		get_stylesheet_directory_uri() . '/style.css',
+		array( 'neom-city-parent-style' ),
+		wp_get_theme()->get('Version')
+	);
+
+	// Styl inline z kolorami (jeśli chcesz nadpisać kolory)
+	// $custom_css = '
+    // :root {
+    // }';
+	// wp_add_inline_style( 'neom-city-child-style', $custom_css );
+}
+add_action( 'wp_enqueue_scripts', 'neom_child_theme_enqueue_styles', 20 );
+
+
+// Hook the enqueue styles function with a higher priority.
+add_action( 'wp_enqueue_scripts', 'neom_child_theme_enqueue_styles', 20 );
